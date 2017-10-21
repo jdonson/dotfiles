4b1be98ba0d16f7faa73503522a7c41d2b737edc
Alex Merenda <amerenda89@gmail.com>
2017-10-21 12:32:15 -0400

    checks if spotify is running and pauses it
    script would fail if spotify was not running

--- bin/fuzzy_lock.sh
+++ bin/fuzzy_lock.sh
@@ -7,8 +7,9 @@ scrot /tmp/screen_locked.png
 mogrify -scale 20% -scale 500% /tmp/screen_locked.png
 
 # Pauses music
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
-
+if pgrep spotify >/dev/null; then
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+fi
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
 
