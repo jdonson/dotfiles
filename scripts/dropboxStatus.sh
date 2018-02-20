ded80a5bbff25eff042a162c62243351df1c7b1a
Alex Merenda <amerenda89@gmail.com>
2018-02-20 13:35:05 -0500

    moves helper scripts to folder outside of path

--- scripts/dropboxStatus.sh
+++ scripts/dropboxStatus.sh
@@ -4,18 +4,15 @@
 #Dropbox is startin: "Starting..."
 #Dropbox is Syncing: "Syncing ..."
 #Dropbox is running: "Up to date..."
-STATUS="$(echo `dropbox status` | awk '{print $1;}')"
-C_status=#fbf1c7
+STATUS="$(echo `dropbox.py status` | awk '{print $1;}')"
 DROPBOX_ICON=""
 if [[ $STATUS == *"Dropbox"* ]]; then
-  C_status=#ec644b
+    :
 elif [[ $STATUS == *"Starting"* ]]; then
-  #statements
   DROPBOX_ICON=""
 elif [[ $STATUS == *"Syncing"* ]]; then
   DROPBOX_ICON=
 else
   DROPBOX_ICON=""
 fi
-echo "%{F$C_status}$DROPBOX_ICON%{-F}"
-#echo "%{A:dropbox star:}$DROPBOX_ICON $ICON%{A}"
+echo "$DROPBOX_ICON"
--- scripts/spotifyCurrent.sh
+++ scripts/spotifyCurrent.sh
@@ -5,10 +5,10 @@ if [[ $? -eq 0 ]]; then
   song_title=$(playerctl metadata title | awk -v len=20 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }')
     if [[ $player_status == "Playing" ]]; then
       metadata="$song_title by $artist"
-      controls="%{A1:playerctl play-pause:} %{A}"
+      controls="%{A1:playerctl play-pause:} %{A}"
     else
       metadata="$song_title by $artist"
-      controls="%{A1:playerctl play-pause:} %{A}"
+      controls="%{A1:playerctl play-pause:} %{A}"
     fi
 fi
 printf "%-5s%-35s" "$controls " "$metadata"
