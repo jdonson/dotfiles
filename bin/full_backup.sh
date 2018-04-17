8544cca9376b47a674d9376cec6b0bfd99c4b601
Alex Merenda <amerenda89@gmail.com>
2018-04-17 11:26:08 -0400

    adds exclude dirs to keep backup reasonable

--- bin/full_backup.sh
+++ bin/full_backup.sh
@@ -9,4 +9,4 @@ if [ $# -eq 0 ]
     echo "please specify a backup path"
 fi
 
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/amerenda/.local/share/Steam/*","/home/amerenda/Dropbox/*","$1"/*} / "$1"
+rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/amerenda/.config/","/home/amerenda/.cache/","/home/amerenda/.local/share/Steam/*","/home/amerenda/tmp/","/home/amerenda/Downloads/","/home/amerenda/.i3/log/","/home/amerenda/.mozilla/","/home/amerenda/Dropbox/*","$1"/*} / "$1"
