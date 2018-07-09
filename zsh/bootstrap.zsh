2cdc0c29b086aec8a04296c87f90daee14db7a24
Alex Merenda <amerenda89@gmail.com>
2018-07-09 12:18:17 -0400

    comments out keychain for now

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -1,1 +1,1 @@
-eval $(keychain --eval --quiet github_deploy id_rsa_amerenda_personal id_rsa_amerenda_placeiq)
+#eval $(keychain --eval --quiet github_deploy id_rsa_amerenda_personal id_rsa_amerenda_placeiq)
