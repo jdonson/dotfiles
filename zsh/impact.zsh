37f6b2cec56eb2505e123a91bf15c78e7306790f
Alex Merenda <amerenda89@gmail.com>
2018-07-11 15:27:24 -0400

    supresses socks bastion messages on new term

--- zsh/impact.zsh
+++ zsh/impact.zsh
@@ -7,7 +7,7 @@ if [ -f '/Users/alexmerenda/bin/platform/google-cloud-sdk/path.zsh.inc' ]; then 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/alexmerenda/bin/platform/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/alexmerenda/bin/platform/google-cloud-sdk/completion.zsh.inc'; fi
 
-ssh -O check bastion
+ssh -O check bastion 2> /dev/null
 
 retVal=$?
 
@@ -16,5 +16,5 @@ if [ $retVal -ne 0 ]; then
    # forward 1080 via socks
    ssh -fNTMn -D 1080 bastion
 else
-   echo "Socks tunnel to bastion is already open..."
+   :
 fi
