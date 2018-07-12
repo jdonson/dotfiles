739b7cceb721699e91b0118671c9230fef2ff51e
Alex Merenda <amerenda89@gmail.com>
2018-07-12 18:42:20 -0400

    blackholes sites via localhost redirect
    uses an array to manage list of sites to block
    can easily add and remove sites

--- bin/focus.sh
+++ bin/focus.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
+
+function blackHoleSites()
+{
+    # back up /etc/hosts for later
+    sudo /bin/cp -f /etc/hosts  /etc/hosts-orginal
+    # clean temp hosts
+    echo > /tmp/hosts.tmp
+    # put /etc/hosts into tmp hosts
+    cat /etc/hosts >> /tmp/hosts.tmp
+    echo "### START BLOCKLIST" >> /tmp/hosts.tmp
+    # loop through sitesToBlock and store it in tmp hosts file
+    for i in "${sitesToBlock[@]}"
+    do
+        echo "127.0.0.1         $i" >> /tmp/hosts.tmp
+        echo "127.0.0.1         www.$i" >> /tmp/hosts.tmp
+        echo "fe80::1%lo0       $i" >> /tmp/hosts.tmp
+        echo "fe80::1%lo0       www.$i" >> /tmp/hosts.tmp
+    done
+    echo "### END BLOCKLIST" >> /tmp/hosts.tmp
+    sudo /bin/cp -f /tmp/hosts.tmp /etc/hosts
+}
+
+declare -a sitesToBlock=("reddit.com" "facebook.com" "news.ycombinator.com")
+
 if [[ ${1} == "start" ]]; then
     osascript -e "display notification \"Time to focus! for ${2} minutes\" with title \"Focus\""
+    blackHoleSites
 elif [[ ${1} == "stop" ]]; then
-    say "Time is up"
+    say "Time is up" &
     osascript -e "display notification \"Time is up! You focused for ${2} minutes\" with title \"Focus\""
+    sudo /bin/cp -f /etc/hosts-orginal /etc/hosts
 fi
