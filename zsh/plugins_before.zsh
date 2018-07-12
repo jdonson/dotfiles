acf96479a358fa6c76f953ff47be6e775f552263
Alex Merenda <amerenda89@gmail.com>
2018-07-12 11:28:21 -0400

    comments out antigen stuff

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -1,5 +1,4 @@
 # External plugins (initialized before)
-source /usr/local/Cellar/antigen/2.2.3/share/antigen/antigen.zsh
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-antigen apply
+#source /usr/local/Cellar/antigen/2.2.3/share/antigen/antigen.zsh
+#
+#antigen apply
