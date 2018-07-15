d402a7cb6db2ac3d9828604eeed250c3c8d9e0e5
Alex Merenda <amerenda89@gmail.com>
2018-07-15 10:33:34 -0400

    adds go config

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,4 +1,5 @@
-export GOPATH=$HOME/go # don't forget to change your path correctly!
+export GOPATH=$HOME/projects/go # don't forget to change your path correctly!
 export GOROOT=/usr/local/opt/go/libexec
+export GOBIN=$HOME/projects/go/bin
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
