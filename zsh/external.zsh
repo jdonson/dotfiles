4fb0ed08518ba1b5e1c4fa10c65ef6e6c550034a
Alex Merenda <amerenda89@gmail.com>
2018-07-15 10:33:25 -0400

    removes go config from external

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -7,7 +7,3 @@
 # Cache pip-installed packages to avoid re-downloading
 export ANSIBLE_NOCOWS=1
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-export GOROOT=/usr/local/go
-export GOBIN=/usr/bin/go
-export GOPATH=~/go
-#export PATH=/home/amerenda/.bin/pip/bin:/home/amerenda/.bin:/home/amerenda/.local/bin/:/usr/local/bin:${PATH}:${GOPATH}
