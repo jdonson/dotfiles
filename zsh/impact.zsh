64a3e12c514e1ad1f30f4e03e391409e941c2795
Alex Merenda <amerenda89@gmail.com>
2018-07-18 12:18:30 -0400

    comments out auto bastion connection

--- zsh/impact.zsh
+++ zsh/impact.zsh
@@ -7,14 +7,14 @@ if [ -f '/Users/alexmerenda/bin/platform/google-cloud-sdk/path.zsh.inc' ]; then 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/alexmerenda/bin/platform/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/alexmerenda/bin/platform/google-cloud-sdk/completion.zsh.inc'; fi
 
-ssh -O check bastion 2> /dev/null
-
-retVal=$?
-
-if [ $retVal -ne 0 ]; then
-   echo "Created socks tunnel to bastion..."
-   # forward 1080 via socks
-   ssh -fNTMn -D 1080 bastion
-else
-   :
-fi
+#ssh -O check bastion 2> /dev/null
+#
+#retVal=$?
+#
+#if [ $retVal -ne 0 ]; then
+#   echo "Created socks tunnel to bastion..."
+#   # forward 1080 via socks
+#   ssh -fNTMn -D 1080 bastion
+#else
+#   :
+#fi
