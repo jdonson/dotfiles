9f9227dc6c619d9079774bde542204df2463b30b
Alex Merenda <amerenda89@gmail.com>
2018-07-18 12:17:37 -0400

    adds coreutils to path

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -7,3 +7,6 @@
 # Cache pip-installed packages to avoid re-downloading
 export ANSIBLE_NOCOWS=1
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+export DEFAULT_USER=alexmerenda
+
+export PATH=$PATH:/usr/local/Cellar/coreutils/8.30/bin
